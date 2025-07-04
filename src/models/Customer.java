89600e701b41002c3241fb140b326c5b8e7654d4
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-04 22:39:04 +0300

    add customer model

---
+++ src/models/Customer.java
@@ -0,0 +1,32 @@
+package models;
+
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalcance(double balance){
+        this.balance = balance;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void deductBalance(double amount) {
+        if (balance < amount) throw new IllegalStateException("Insufficient balance.");
+        balance -= amount;
+    }
+}
